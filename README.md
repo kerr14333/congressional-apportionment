39b9260811f163dd4af4e3975c16755a0f3ba82b
Grieves <Grieves.Christopher@bls.gov>
2019-01-04 14:54:47 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Congressional Apportionment 1910-2010
+
+![Congressional Apportionment](http://github.com/kerr14333/congressional-apportionment/img/apportionment.png)
+
+## Data Sources
+[Apportionment of Membership of the House of Representatives: 1789-2000 ](https://www.census.gov/population/apportionment/data/2000_apportionment_results.html)
+[Apportionment Population and Number of Representatives, by State: 2010 Census](https://www.census.gov/population/apportionment/data/2010_apportionment_results.html)
+
+## Tools
+
+I used ggplot in R to create the graph.
+
+## Graph Info
+
+1910 to 2010 was chosen due to there being a constant 435 representatives with the exception of the 1950 census when there were briefly 437. This was due to the introduction of Hawaii and Alaska as states.
+
+The subset of states was chosen to be states where the range of seats over the time period was greater or equal to 8. This was chosen for two reasons. One, it did not clutter up the graph and two, it showed states where there has been a large change in its representation.
+
+Feel free to play around with the code and see what you can do with it yourself.
---
+++ apportionment.R
@@ -0,0 +1,45 @@
+#Libraries
+library(tidyverse)
+library(ggrepel)
+
+
+#Sources:
+#https://www.census.gov/population/apportionment/data/2000_apportionment_results.html
+#https://www.census.gov/population/apportionment/data/2010_apportionment_results.html
+
+#Read in data
+historic_data <- read_csv("apportionment.csv")
+
+#Subset Data and Create My Labels
+sub<-historic_data %>% 
+  group_by(State) %>% 
+  filter(all(abs(max(seats)-min(seats))>=8)) %>%
+  mutate(label = if_else(year == max(year), 
+                         paste0(as.character(State)," ",seats), 
+                         NA_character_))
+
+  #Plot the data
+  sub %>%
+  ggplot(aes(x=year,y=seats,col=as.factor(State),group=State)) + 
+  geom_line() + 
+  guides(col=F) + #Remove guide because it will clutter
+  ggtitle("Apportionment of Congressional Seats\n1910-2010")+
+  labs(subtitle="States with a range of 8 or greater \nseats over the time period.",
+       caption="Source: U.S. Census Bureau.")+
+  xlab("Year") + 
+  ylab("Number of Seats") +
+  theme(legend.position = "bottom",
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1))+
+  scale_x_continuous(breaks=my.breaks,limits=c(1910,2060)) +
+  scale_y_continuous(breaks=seq(from=0,to=55,by=5),limits=c(0,55)) +
+  geom_label_repel(aes(label = label), #Add Labels
+                   nudge_x = 50,
+                   label.size = 0,
+                   label.padding = 0.15,
+                   direction="y",
+                   na.rm = TRUE,segment.colour = "black",
+                   arrow = arrow(length = unit(0.02, "npc"))) 
+  
+
